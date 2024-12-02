41ed2428b4de94193d632409fc89a7127e25a566
VictorErikson <victoreriksson_93@hotmail.com>
2024-12-02 14:24:58 +0100

    Initial commit

---
+++ JS_pages/hemkunskap.js
@@ -0,0 +1,9 @@
+
+//när klar visa vilka svar som var rätt/fel + byta ut citatet mot
+
+
+// resultat < 20 → Du är tyvärr inte riktigt lika klipskt som Diana Moseni och Diego Martinez Parra. (bilder deras huvuden)
+
+// resultat = 20 →lika smart
+
+// resultat > → Wow! Du är smartare än Diana Moseni och Diego Martinez Parra, snyggt!
---
+++ JS_pages/svenska.js
@@ -0,0 +1,9 @@
+
+
+//när klar visa vilka svar som var rätt/fel + byta ut citatet mot
+
+// resultat < 11 → Du är tyvärr inte riktigt lika klipskt som Mattias Helén(bilder huvud)
+
+// resultat = 11 →lika smart
+
+// resultat > 11 → Wow! Du är smartare än Mattias Helén, snyggt!
